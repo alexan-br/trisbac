ebfbe1d737f6b5842c0c34dd604602ce0859458e
MathisPoissel <mathis.poissel@gmail.com>
2024-06-06 15:54:33 +0200

    Feat: cellularItem

--- src/app/components/CellulareItem/cellularItem.js
+++ src/app/components/CellulareItem/cellularItem.js
@@ -1,2 +1,11 @@
 import React from "react";
 import Styles from "./cellularItem.module.scss";
+
+export default function CellularItem({ cellular }) {
+    return (
+        <div className={Styles.cellularItem}>
+            <h2>{cellular.name}</h2>
+            <p>{cellular.description}</p>
+        </div>
+    );
+}
