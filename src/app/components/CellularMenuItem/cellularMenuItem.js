cd12a9901777573c8f6a946068f482e30728931a
MathisPoissel <mathis.poissel@gmail.com>
2024-06-13 11:50:52 +0200

    Feat: Menu Hom Items Positionements

--- src/app/components/CellularMenuItem/cellularMenuItem.js
+++ src/app/components/CellularMenuItem/cellularMenuItem.js
@@ -2,7 +2,7 @@ import Styles from "./cellularMenuItem.module.scss";
 
 export default function cellularMenuItem({ cellular }) {
   return (
-    <div className={Styles.cellularItem}>
+    <div className={`${Styles.cellularItem} CellularMenuItem`}>
       <div className={Styles.cellularMask}>
         <img src={cellular.iconLink} alt={cellular.name} />
         <h2>{cellular.name}</h2>
